13c5f43459f2c946f9a35cc4ed5625cfa8081bc2
Chris Alfano <chris@jarv.us>
2019-07-13 15:27:52 -0400

    feat: add icon

--- html-templates/trello/power-up.tpl
+++ html-templates/trello/power-up.tpl
@@ -6,8 +6,9 @@
     <body>
         <script src="https://p.trellocdn.com/power-up.min.js"></script>
         <script>
-            const JARVUS_KEY = {Jarvus\Trello\RequestHandler::$powerUpKey|json_encode};
-            const TRELLO_APP_KEY = {Jarvus\Trello\API::$key|json_encode};
+            window.JARVUS_KEY = {Jarvus\Trello\RequestHandler::$powerUpKey|json_encode};
+            window.TRELLO_APP_KEY = {Jarvus\Trello\API::$key|json_encode};
+            window.ICON_COINS = '{versioned_url img/coins.svg absolute=true}';
         </script>
         <script src="{versioned_url js/trello/power-up.js}"></script>
     </body>
--- site-root/js/trello/power-up.js
+++ site-root/js/trello/power-up.js
@@ -2,35 +2,27 @@
 TrelloPowerUp.initialize({
     'board-buttons': t => [
         {
-            // we can either provide a button that has a callback function
-            // icon: {
-            //   dark: 'https://jarv.us/img/glyph/j-logo.svg',
-            //   light: 'https://jarv.us/img/glyph/j-logo.svg'
-            // },
-            text: 'Label Totals',
+            icon: window.ICON_COINS,
+            text: 'Labels',
             condition: 'always',
             callback: t => t.board('id').then(board => t.popup({
                 title: 'Field Totals by Label',
-                url: `https://trello.jarv.us/trello/${board.id}/label-totals?key=${JARVUS_KEY}`,
+                url: `https://trello.jarv.us/trello/${board.id}/label-totals?key=${window.JARVUS_KEY}`,
                 height: 400
             }))
         },
         {
-            // we can either provide a button that has a callback function
-            // icon: {
-            //   dark: 'https://jarv.us/img/glyph/j-logo.svg',
-            //   light: 'https://jarv.us/img/glyph/j-logo.svg'
-            // },
-            text: 'Board Totals',
+            icon: window.ICON_COINS,
+            text: 'Board',
             condition: 'always',
             callback: t => t.board('id').then(board => t.popup({
                 title: 'Field Totals',
-                url: `https://trello.jarv.us/trello/${board.id}/field-totals?key=${JARVUS_KEY}`,
+                url: `https://trello.jarv.us/trello/${board.id}/field-totals?key=${window.JARVUS_KEY}`,
                 height: 200
             }))
         }
     ]
 }, {
-    appKey: TRELLO_APP_KEY,
+    appKey: window.TRELLO_APP_KEY,
     appName: 'Jarvus'
 });
